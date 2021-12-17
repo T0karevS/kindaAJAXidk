a78cf04d38f2ce604f91c5ef265361301f41570f
T0karevS <90570998+T0karevS@users.noreply.github.com>
2021-12-17 10:04:05 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -10,7 +10,15 @@
     <form method="post" class="form" enctype="multipart/form-data">
         <input type="text" name="text">
         <button type="submit" name="btn"> Отправить</button>
+    </form>
+    <form name="form2" id="form2">
+        <?php
+        require_once 'send.php ';
 
+        $posts = getPost();
+        require_once "showposts.php";
+
+        ?>
     </form>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="ajax.js" ></script>
--- send.php
+++ send.php
@@ -2,5 +2,12 @@
     $pdo = new PDO( "mysql:host = localhost;dbname=rp32ajax", "root", "root");
     $bd = "INSERT INTO text (text) VALUE (:text)";
     $statement = $pdo->prepare($bd);
-    $statement->execute(['text'=>$_POST['text']])
-    ?>
+    $statement->execute(['text'=>$_POST['text']]);
+function getPost()
+{
+    $pdo = new PDO( "mysql:host = localhost;dbname=rp32ajax", "root", "root");
+    $statement = $pdo->prepare("SELECT * FROM text");
+    $statement->execute();
+    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $posts;
+}
