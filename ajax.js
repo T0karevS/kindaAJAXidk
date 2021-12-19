87c3617143d5fb7ca9b81739c1cffffc8553a6c3
T0karevS <90570998+T0karevS@users.noreply.github.com>
2021-12-20 00:31:16 +0400

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -2,7 +2,6 @@ $('.form').submit(function(e){
 e.preventDefault();
 let th=$(this);
 let btn = th.find('.btn');
-btn.addClass('progress-bar-striped progress-bar-animated')
 
 $.ajax({
 	url: '/send.php',
@@ -10,7 +9,6 @@ $.ajax({
 	dataType: 'html',
     data: th.serialize(),
 	success: function(data){
-		alert("success");
 		document.querySelector(".info").innerHTML = data;
 	}
 	
